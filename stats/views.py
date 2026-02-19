5490b53566f18073eeb9027647203641029fae96
Sir-bastian <sebamadziwanzira@gmail.com>
2026-02-19 22:51:11 +0200

    Base template created

--- stats/views.py
+++ stats/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. Welcome to ZPSL Stats Hub - Coming soon.")
+
+def standings(request):
+    return HttpResponse("ZPSL Standings - Coming Soon!")
