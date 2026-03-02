98cac7c907624abfae811ed6487d9f0bf431f486
Sir-bastian <sebamadziwanzira@gmail.com>
2026-03-02 23:59:35 +0200

    Results and Fixtures feature page

--- stats/urls.py
+++ stats/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('/', views.index, name='index'),
     path('standings/', views.standings, name='standings'),
+    path('results-and-fixtures/', views.resultsAndFixtures, name='results_and_fixtures'),
 ]
--- stats/views.py
+++ stats/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . models import Team
+from . models import Team, Match
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. Welcome to ZPSL Stats Hub - Coming soon.")
+    return HttpResponse("<h1>Hello, world. Welcome to ZPSL Stats Hub - Coming soon.</h1>")
 
 def standings(request):
     teams = Team.objects.all()
     sorted_teams = sorted(teams, key=lambda t: (t.points, t.goal_difference), reverse=True)
 
     return render(request, 'stats/standings.html', {'teams': sorted_teams})
+
+def resultsAndFixtures(request):
+    ''' A view that dsiplay the recent and past match results'''
+
+    results = Match.objects.filter(match_status='Completed').select_related('home_team', 'away_team').order_by('-date')[:10]
+    fixtures = Match.objects.filter(match_status='Scheduled').select_related('home_team', 'away_team').order_by('date')[:10]
+
+    context = {
+        'results': results,
+        'fixtures': fixtures
+    }
+
+    return render(request, 'stats/results_and_fixtures.html', context)
