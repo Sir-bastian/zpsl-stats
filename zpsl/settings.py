f66de86fc2f5c978e0c3467a1808a07f92c7f76a
Sir-bastian <sebamadziwanzira@gmail.com>
2026-04-18 22:46:23 +0200

    Allow render as host

--- zpsl/settings.py
+++ zpsl/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-c*j=8vf7(gpo*=08dks^8io4z(8=%1#^8m@h!zj#lb(b=-j6el
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zpsl-stats.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
