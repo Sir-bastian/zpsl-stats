1f5e3ed385ff00f624c59c0fb31ab388016fe599
Sir-bastian <sebamadziwanzira@gmail.com>
2026-04-22 12:57:47 +0200

    Added static files path

--- zpsl/settings.py
+++ zpsl/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# If you have a custom static folder in your project, ensure this is there too:
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
